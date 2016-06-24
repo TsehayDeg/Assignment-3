e155cf2e30ca3c0a03ade3e71c1987fa6fc48eab
TsehayDeg <TsehayDeg@yahoo.com>
2016-06-24 13:43:40 -0700

    first commit

---
+++ Assignment3.js
@@ -0,0 +1,77 @@
+//Q 1
+/*var movies = ["Goal", "Men of Honor", "Peaceful Warrior", "Jhon Q", "My Odipus Complex"];
+console.log(movies[1]);*/
+
+//Q 2
+/*var movies = [];
+movies[0] = "Goal";
+movies[1] = "Men of Honor";
+movies[2] = "Peaceful Warrior";
+movies[3] = "Jhon Q";
+movies[4] = "My Odipus Complex";
+console.log(movies[0]); */
+
+
+//Q 3
+/*var movies = ["Goal", "Men of Honor", "Peaceful Warrior", "Jhon Q", "My Odipus Complex"];
+movies.splice(2, 0, "Acapulco Bay");
+console.log(movies.length);*/
+//Q 4
+/*var movies = [];
+movies[0] = "Goal";
+movies[1] = "Men of Honor";
+movies[2] = "Peaceful Warrior";
+movies[3] = "Jhon Q";
+movies[4] = "My Odipus Complex";
+movies.shift();
+console.log(movies);*/
+
+//Q 5
+/*var movies = [];
+movies[0] = "Goal";
+movies[1] = "Men of Honor";
+movies[2] = "Peaceful Warrior";
+movies[3] = "Jhon Q";
+movies[4] = "My Odipus Complex";
+movies[5] = "Acapulco Bay";
+movies[6] = "Finding Nimo";
+var text = "";
+var i;
+for (i = 0; i < movies.length; i++) {
+    text = movies[i];
+    console.log(text);
+}*/
+
+
+
+//Q 6
+/*var movies = ["Goal", "Men of Honor", "Peaceful Warrior", "Jhon Q", "My Odipus Complex"];
+var text = "";
+var i;
+for (i in movies) {
+    text += movies[i] + ", ";
+}
+console.log(text);*/
+
+//Q 7
+/*var movies = ["Goal", "Men of Honor", "Peaceful Warrior", "Jhon Q", "My Odipus Complex"];
+var text = "";
+var i;
+for (i in movies) {
+    text = movies[i];
+    console.log(text);
+}*/
+
+//Q 8?
+/*var movies = ["Goal", "Men of Honor", "Peaceful Warrior", "Jhon Q", "My Odipus Complex"];
+var leastFavMovies = ["Space 1999", "Acapulco Bay"];
+var text = "";
+var i;
+for (i in movies) {
+    text = movies[i];
+    console.log("Movies I like:" + text); 
+}*/
+
+//Q 9
+var movies = ["Goal", "Men of Honor", "Peaceful Warrior", "Jhon Q", "My Odipus Complex"];
+var leastFavMovies = ["Space 1999", "Acapulco Bay"];
